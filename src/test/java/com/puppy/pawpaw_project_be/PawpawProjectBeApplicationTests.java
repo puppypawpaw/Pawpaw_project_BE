a75f44aa793b6f91b5bfdf5c72fbbb295e9d6596
Chaseoungjun <chas369@naver.com>
2023-07-25 22:10:25 +0900

    기본 설정

---
+++ src/test/java/com/puppy/pawpaw_project_be/PawpawProjectBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puppy.pawpaw_project_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawpawProjectBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
