86ebc5fa70316b353c6fae807d3cb81b4ce2763c
Chaseoungjun <chas369@naver.com>
2023-08-08 17:39:37 +0900

    #12 게시글 등록, 수정, 응답을 위한 DTO 클래스 생성

---
+++ src/main/java/com/puppy/pawpaw_project_be/domain/board/dto/BoardDto.java
@@ -0,0 +1,68 @@
+package com.puppy.pawpaw_project_be.domain.board.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class BoardDto {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class BoardRegisterDto {
+        private Long id;
+        private String title;
+        private String content;
+        private String writer;
+        private List<String> fileNames;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class BoardResponseDto {
+
+        private String title;
+        private String content;
+
+        private String writer;
+
+        private boolean isRemoved = false;
+
+        private int viewCount;
+
+        private int likedCount;
+
+        private LocalDateTime createdAt = LocalDateTime.now();
+
+        private LocalDateTime updatedAt = LocalDateTime.now();
+
+        private List<String> commentList = new ArrayList<>();
+
+        //첨부파일의 이름들
+        private List<String> fileNames;
+
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public static class BoardUpdateDto {
+        private String title;
+        private String content;
+        private List<String> fileNames;
+
+        @Builder
+        public BoardUpdateDto(String title, String content) {
+            this.title = title;
+            this.content = content;
+        }
+    }
+
+}
