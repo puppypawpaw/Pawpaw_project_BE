6a94cf25a4a7711ce063667ba7da3e0178aa3067
Chaseoungjun <chas369@naver.com>
2023-08-14 13:50:24 +0900

    #20 부모 클래스인 BusinessException 으로 다른 예외들의 처리가 가능하기 때문에 각 예외별로 작성했던 핸들러 코드들을 삭제

--- src/main/java/com/puppy/pawpaw_project_be/exception/common/GlobalExceptionHandler.java
+++ src/main/java/com/puppy/pawpaw_project_be/exception/common/GlobalExceptionHandler.java
@@ -1,17 +1,5 @@
 package com.puppy.pawpaw_project_be.exception.common;
 
-import com.puppy.pawpaw_project_be.exception.board.BoardException.BoardDeleteException;
-import com.puppy.pawpaw_project_be.exception.board.BoardException.BoardNotFoundException;
-import com.puppy.pawpaw_project_be.exception.board.BoardException.BoardRegisterException;
-import com.puppy.pawpaw_project_be.exception.board.BoardException.BoardUpdateException;
-import com.puppy.pawpaw_project_be.exception.board.BoardImgException.BoardImgCanNotRemoveException;
-import com.puppy.pawpaw_project_be.exception.board.BoardImgException.BoardImgCanNotUploadException;
-import com.puppy.pawpaw_project_be.exception.board.BoardImgException.BoardImgCanNotViewException;
-import com.puppy.pawpaw_project_be.exception.board.BoardLikeException.BoardDeleteLikeFailException;
-import com.puppy.pawpaw_project_be.exception.board.BoardLikeException.BoardLikeFailException;
-import com.puppy.pawpaw_project_be.exception.comment.CommentException.CommentDeleteException;
-import com.puppy.pawpaw_project_be.exception.comment.CommentException.CommentRegisterException;
-import com.puppy.pawpaw_project_be.exception.comment.CommentException.CommentUpdaterException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -47,44 +35,4 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(ErrorResponse.of(ErrorCode.HANDLE_AUTHENTICATION_ENTRYPOINT), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(BoardNotFoundException.class)
-    protected ResponseEntity<ErrorResponse> handleBoardNotFoundException(BoardNotFoundException e){
-        log.error("BoardRegisterException", e);
-        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.BOARD_NOTFOUND_EXCEPTION), HttpStatus.BAD_REQUEST);
-    }
-    @ExceptionHandler(BoardRegisterException.class)
-    protected ResponseEntity<ErrorResponse> handleBoardRegistrationException(BoardRegisterException e){
-        log.error("BoardRegisterException", e);
-        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.BOARD_REGISTRATION_EXCEPTION), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(BoardUpdateException.class)
-    protected ResponseEntity<ErrorResponse> handleBoardUpdateException(BoardUpdateException e){
-        log.error("BoardUpdateException", e);
-        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.BOARD_UPDATE_EXCEPTION), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(BoardDeleteException.class)
-    protected ResponseEntity<ErrorResponse> handleBoardDeletionException(BoardDeleteException e){
-        log.error("BoardDeleteException", e);
-        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.BOARD_DELETE_EXCEPTION), HttpStatus.BAD_REQUEST);
-    }
-
-
-    @ExceptionHandler(BoardImgCanNotUploadException.class)
-    protected ResponseEntity<ErrorResponse> handleBoardImgCanNotUploadException(BoardImgCanNotUploadException e){
-        log.error("BoardDeleteLikeFailException", e);
-        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.BOARD_IMG_CANNOT_UPLOAD_EXCEPTION), HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(BoardImgCanNotViewException.class)
-    protected ResponseEntity<ErrorResponse> handleBoardImgCanNotViewException(BoardImgCanNotViewException e){
-        log.error("BoardImgCanNotViewException", e);
-        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.BOARD_IMG_CANNOT_VIEW_EXCEPTION), HttpStatus.BAD_REQUEST);
-    }
-    @ExceptionHandler(BoardImgCanNotRemoveException.class)
-    protected ResponseEntity<ErrorResponse> handleBoardImgCanNotRemoveException(BoardImgCanNotRemoveException e){
-        log.error("BoardImgCanNotRemoveException", e);
-        return new ResponseEntity<>(ErrorResponse.of(ErrorCode.BOARD_IMG_CANNOT_REMOVE_EXCEPTION), HttpStatus.BAD_REQUEST);
-    }
 }
