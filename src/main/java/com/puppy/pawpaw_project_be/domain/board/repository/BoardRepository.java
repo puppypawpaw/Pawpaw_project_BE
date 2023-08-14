c0002258ab9373df31f1127b672d594529652754
Chaseoungjun <chas369@naver.com>
2023-08-14 13:49:07 +0900

    #20 빌드 에러 방지를 위해 @Param 추가

--- src/main/java/com/puppy/pawpaw_project_be/domain/board/repository/BoardRepository.java
+++ src/main/java/com/puppy/pawpaw_project_be/domain/board/repository/BoardRepository.java
@@ -4,6 +4,7 @@ import com.puppy.pawpaw_project_be.domain.board.entity.Board;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,5 +12,5 @@ public interface BoardRepository extends JpaRepository<Board, Long>{
 
     @EntityGraph(attributePaths = {"imgSet"}) // board와  Lazy 연관관계를 맺고 있는 img도 한번에 가져오게함
     @Query("select b from Board b where b.id =:id")
-    Optional<Board> findByIdWithImages(Long id);
+    Optional<Board> findByIdWithImages(@Param("id") Long id);
 }
