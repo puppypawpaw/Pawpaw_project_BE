e90b0c48f9e84cf362748cd94fdabbbe20106a8c
Seungjun Cha <chas369@naver.com>
2023-08-20 22:54:22 +0900

    #29 게시글 CRUD의 예외코드 추가

---
+++ common/src/main/java/kr/co/pawpaw/common/exception/board/BoardException.java
@@ -0,0 +1,33 @@
+package kr.co.pawpaw.common.exception.board;
+
+
+import kr.co.pawpaw.common.exception.common.BusinessException;
+import kr.co.pawpaw.common.exception.common.ErrorCode;
+
+public class BoardException{
+
+
+    public static class BoardNotFoundException extends BusinessException {
+        public BoardNotFoundException() {
+            super(ErrorCode.BOARD_NOTFOUND_EXCEPTION);
+        }
+    }
+
+    public static class BoardRegisterException extends BusinessException {
+        public BoardRegisterException() {
+            super(ErrorCode.BOARD_REGISTRATION_EXCEPTION);
+        }
+    }
+
+    public static class BoardUpdateException extends BusinessException{
+        public BoardUpdateException() {
+            super(ErrorCode.BOARD_UPDATE_EXCEPTION);
+        }
+    }
+
+    public static class BoardDeleteException extends BusinessException{
+        public BoardDeleteException() {
+            super(ErrorCode.BOARD_DELETE_EXCEPTION);
+        }
+    }
+}
