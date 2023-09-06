8090b9b08ee351ccbb0dcf495a2277e81404fa2d
Seungjun Cha <chas369@naver.com>
2023-09-06 14:55:31 +0900

    #40 게시글 조회에 필요한 dto클래스 생성, Reply 등록,수정,조회에 필요한 dto클래스 생성

--- api/src/main/java/kr/co/pawpaw/api/dto/board/BoardDto.java
+++ api/src/main/java/kr/co/pawpaw/api/dto/board/BoardDto.java
@@ -1,10 +1,12 @@
 package kr.co.pawpaw.api.dto.board;
 
+import kr.co.pawpaw.api.dto.reply.ReplyDto.ReplyListDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 public class BoardDto {
@@ -52,6 +54,33 @@ public class BoardDto {
         public BoardUpdateDto(String title, String content) {
             this.title = title;
             this.content = content;
+        }
+    }
+
+    @Getter
+    @Setter
+    public static class BoardListDto {
+        private Long id;
+        private String title;
+        private String content;
+        private String writer;
+        private List<ReplyListDto> replyListDto;
+        private int likedCount;
+        private int replyCount;
+        private LocalDateTime createdDate;
+        private LocalDateTime modifiedDate;
+
+        @Builder
+        public BoardListDto(Long id, String title, String content, String writer, int likedCount, int replyCount, LocalDateTime createdDate, LocalDateTime modifiedDate, List<ReplyListDto> replyListDto) {
+            this.id = id;
+            this.title = title;
+            this.content = content;
+            this.writer = writer;
+            this.likedCount = likedCount;
+            this.replyCount = replyCount;
+            this.createdDate = createdDate;
+            this.modifiedDate = modifiedDate;
+            this.replyListDto = replyListDto;
         }
     }
 
---
+++ api/src/main/java/kr/co/pawpaw/api/dto/reply/ReplyDto.java
@@ -0,0 +1,60 @@
+package kr.co.pawpaw.api.dto.reply;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReplyDto {
+
+    @Getter
+    @Setter
+    public static class ReplyRegisterDto {
+        private Long boardId;
+        private Long parentId;
+        private String content;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class ReplyUpdateDto {
+        private String content;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class ReplyResponseDto {
+        private String writer;
+        private Long boardId;
+        private Long parentId;
+        private Long replyId;
+        private String content;
+
+        @Builder
+        public ReplyResponseDto(String writer, Long boardId, Long parentId, Long replyId, String content) {
+            this.writer = writer;
+            this.boardId = boardId;
+            this.parentId = parentId;
+            this.replyId = replyId;
+            this.content = content;
+        }
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class ReplyListDto {
+        private Long id;
+        private String content;
+        private String nickname;
+        private List<ReplyListDto> children = new ArrayList<>();
+
+        public ReplyListDto(Long id, String content, String nickname) {
+            this.id = id;
+            this.content = content;
+            this.nickname = nickname;
+        }
+    }
+}
