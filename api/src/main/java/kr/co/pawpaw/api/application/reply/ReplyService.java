94e8021dc608ce75238497ea29683c208d35c23d
Seungjun Cha <chas369@naver.com>
2023-09-08 17:16:02 +0900

    #40 댓글 등록은 부모 댓글이 있는 경우와 없는 경우를 메서드로 명확하게 분리하고, N+1문제와 무한루프 문제를 고려하여 코드수정

--- api/src/main/java/kr/co/pawpaw/api/application/reply/ReplyService.java
+++ api/src/main/java/kr/co/pawpaw/api/application/reply/ReplyService.java
@@ -5,7 +5,6 @@ import kr.co.pawpaw.api.dto.reply.ReplyDto.ReplyListDto;
 import kr.co.pawpaw.api.dto.reply.ReplyDto.ReplyRegisterDto;
 import kr.co.pawpaw.api.dto.reply.ReplyDto.ReplyResponseDto;
 import kr.co.pawpaw.common.exception.board.BoardException.BoardNotFoundException;
-import kr.co.pawpaw.common.exception.common.PermissionRequiredException;
 import kr.co.pawpaw.common.exception.reply.ReplyException;
 import kr.co.pawpaw.common.exception.reply.ReplyException.ReplyNotFoundException;
 import kr.co.pawpaw.common.exception.user.NotFoundUserException;
@@ -13,7 +12,6 @@ import kr.co.pawpaw.domainrdb.board.domain.Board;
 import kr.co.pawpaw.domainrdb.board.service.command.BoardCommand;
 import kr.co.pawpaw.domainrdb.board.service.query.BoardQuery;
 import kr.co.pawpaw.domainrdb.reply.domain.Reply;
-import kr.co.pawpaw.domainrdb.reply.repository.ReplyRepository;
 import kr.co.pawpaw.domainrdb.reply.service.command.ReplyCommand;
 import kr.co.pawpaw.domainrdb.reply.service.query.ReplyQuery;
 import kr.co.pawpaw.domainrdb.user.domain.User;
@@ -27,7 +25,6 @@ import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -46,16 +43,24 @@ public class ReplyService {
     private final BoardCommand boardCommand;
     private final ReplyCommand replyCommand;
     private final ReplyQuery replyQuery;
-    private final ReplyRepository replyRepository;
 
     @Transactional
-    public ReplyResponseDto register(@RequestBody ReplyRegisterDto registerDto, UserId userId) {
+    public ReplyResponseDto register(ReplyRegisterDto registerDto, UserId userId) {
         User user = userQuery.findByUserId(userId).orElseThrow(NotFoundUserException::new);
         Board board = boardQuery.findById(registerDto.getBoardId()).orElseThrow(BoardNotFoundException::new);
-        Reply parentReply = null;
-        if (registerDto.getParentId() != null) {
-            parentReply = replyQuery.findById(registerDto.getParentId()).orElseThrow(ReplyNotFoundException::new);
-        }
+        Reply parentReply = replyQuery.findReplyById(registerDto.getParentId()).orElse(null);
+
+        if (registerDto.getContent() == null)
+            throw new ReplyException.ReplyRegisterException();
+
+        if (parentReply == null)
+            return registerReplyWhenNotExistParentReply(registerDto, user, board);
+        else
+            return registerReplyWhenExistParentReply(registerDto, user, board, parentReply);
+    }
+
+    private ReplyResponseDto registerReplyWhenExistParentReply(ReplyRegisterDto registerDto, User user, Board board, Reply parentReply) {
+
         Reply reply = Reply.builder()
                 .user(user)
                 .content(registerDto.getContent())
@@ -63,21 +68,10 @@ public class ReplyService {
                 .board(board)
                 .parent(parentReply)
                 .build();
-        if (reply.getContent() == null) {
-            throw new ReplyException.ReplyRegisterException();
-        }
-        if (user.getUserId() != reply.getUser().getUserId()) {
-            throw new PermissionRequiredException();
-        }
-        Reply savedReply = replyCommand.save(reply);
-        board.plusReplyCount();
-        boardCommand.save(board);
 
-        if (parentReply != null) {
-            parentReply.getChild().add(savedReply); // 부모 댓글에 자식 댓글 추가
-            replyCommand.save(parentReply);
-        }
-        boardCommand.save(board);
+        Reply savedReply = replyCommand.save(reply);
+        parentReply.getChild().add(savedReply); // 부모 댓글에 자식 댓글 추가
+        board.plusReplyCount();
 
         return ReplyResponseDto.builder()
                 .writer(user.getNickname())
@@ -87,51 +81,70 @@ public class ReplyService {
                 .replyId(savedReply.getId())
                 .build();
     }
+    private ReplyResponseDto registerReplyWhenNotExistParentReply(ReplyRegisterDto registerDto, User user, Board board) {
+        Reply reply = Reply.builder()
+                .user(user)
+                .content(registerDto.getContent())
+                .writer(user.getNickname())
+                .board(board)
+                .build();
 
-    @Transactional
-    public ReplyResponseDto update(@RequestBody ReplyDto.ReplyUpdateDto updateDto, UserId userId, Long replyId) {
-        User user = userQuery.findByUserId(userId).orElseThrow(NotFoundUserException::new);
-        Reply reply = replyQuery.findById(replyId).orElseThrow(ReplyNotFoundException::new);
+        Reply savedReply = replyCommand.save(reply);
+        board.plusReplyCount();
 
-        if (updateDto.getContent() == null) {
-            throw new ReplyException.ReplyUpdaterException();
-        }
-        if (user.getUserId() != reply.getUser().getUserId()) {
-            throw new PermissionRequiredException();
-        }
-        if (reply.getParent() != null) {
-            reply.getParent().getChild().forEach(child -> {
-                if (child.getId().equals(reply.getId())) {
-                    child.updateContent(updateDto.getContent());
-                }
-            });
-        } else {
-            reply.updateContent(updateDto.getContent());
-        }
-        replyCommand.save(reply);
         return ReplyResponseDto.builder()
                 .writer(user.getNickname())
-                .content(updateDto.getContent())
-                .replyId(reply.getId())
+                .content(savedReply.getContent())
+                .boardId(registerDto.getBoardId())
+                .parentId(null)
+                .replyId(savedReply.getId())
                 .build();
     }
 
     @Transactional
-    public boolean remove(UserId userId, Long replyId, Long boardId) {
-        User user = userQuery.findByUserId(userId).orElseThrow(NotFoundUserException::new);
-        Board board = boardQuery.findById(boardId).orElseThrow(BoardNotFoundException::new);
-        Reply parentReply = replyQuery.findReplyByIdWithParent(replyId).orElseThrow(ReplyNotFoundException::new);
-        if (user.getUserId() != parentReply.getUser().getUserId()) {
-            throw new PermissionRequiredException();
+    public ReplyResponseDto update(ReplyDto.ReplyUpdateDto updateDto, UserId userId, Long replyId) {
+        Reply reply = replyQuery.findReplyByIdAndUser_UserId(replyId, userId).orElseThrow(ReplyNotFoundException::new);
+
+        if (updateDto.getContent() == null)
+            throw new ReplyException.ReplyUpdaterException();
+
+        if (reply.getParent() != null) {
+            Reply child = findChildById(reply.getParent(), reply.getId());
+            child.updateContent(updateDto.getContent());
+        } else
+            reply.updateContent(updateDto.getContent());
+
+        return ReplyResponseDto.builder()
+                .writer(reply.getWriter())
+                .content(updateDto.getContent())
+                .boardId(reply.getBoard().getId())
+                .replyId(reply.getId())
+                .build();
+    }
+
+    private Reply findChildById(Reply parent, Long childId) {
+        if (parent.getId().equals(childId)) {
+            return parent;
         }
-        parentReply.remove();
-        parentReply.getChild().forEach(child -> {
-//            child.removeParent();
-            child.remove();
-            replyCommand.save(child);
-        });
+        for (Reply child : parent.getChild()) {
+            Reply foundChild = findChildById(child, childId);
+            if (foundChild != null) {
+                return foundChild;
+            }
+        }
+        throw new ReplyNotFoundException();
+    }
+
+    @Transactional
+    public void remove(UserId userId, Long replyId, Long boardId) {
+        Board board = boardQuery.findById(boardId).orElseThrow(BoardNotFoundException::new);
+        Reply parentReply = replyQuery.findReplyByIdWithParentAndUser_UserId(replyId, userId).orElseThrow(ReplyNotFoundException::new);
+
+        List<Reply> replies = replyQuery.findRepliesWithChildren(parentReply);
+        replies.forEach(Reply::remove);
+
+        replyQuery.updateReplyById(replyId);
         board.minusReplyCount();
-        return true;
     }
 
     @Transactional(readOnly = true)
@@ -139,7 +152,7 @@ public class ReplyService {
         userQuery.findByUserId(userId).orElseThrow(NotFoundUserException::new);
         boardQuery.findById(boardId).orElseThrow(BoardNotFoundException::new);
 
-        Slice<Reply> replySlice = replyRepository.findReplyListByBoardId(boardId, pageable);
+        Slice<Reply> replySlice = replyQuery.findReplyListByBoardId(boardId, pageable);
         List<ReplyListDto> replyList = convertNestedStructure(replySlice.getContent());
 
         return new SliceImpl<>(replyList, pageable, replySlice.hasNext());
@@ -167,7 +180,7 @@ public class ReplyService {
             List<ReplyListDto> childDtos = reply.getChild().stream()
                     .map(this::convertCommentToDto)
                     .collect(Collectors.toList());
-            dto.setChildren(childDtos);
+            dto.setChildToParentReply(childDtos);
             return dto;
         }
     }
