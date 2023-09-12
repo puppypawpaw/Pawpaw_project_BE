7bbc53188c26c58d406b3845673a51ad71581738
lkw1211 <tnt7986@gmail.com>
2023-09-12 22:20:25 +0900

    Fix: 채팅방 커버 채팅방에 포함

--- domain/domain-rdb/src/main/java/kr/co/pawpaw/domainrdb/chatroom/repository/ChatroomCoverRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.co.pawpaw.domainrdb.chatroom.repository;
-
-import kr.co.pawpaw.domainrdb.chatroom.domain.ChatroomCover;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatroomCoverRepository extends JpaRepository<ChatroomCover, Long> {
-}
