981948a06c1592b6cc0df73e25cd3ddaa6f781c1
Seungjun Cha <chas369@naver.com>
2023-09-15 02:24:23 +0900

    #44 FileService 를 사용하여 코드를 전체적으로 수정

--- api/src/main/java/kr/co/pawpaw/api/application/boardImg/BoardImgService.java
+++ api/src/main/java/kr/co/pawpaw/api/application/boardImg/BoardImgService.java
@@ -1,125 +1,90 @@
 package kr.co.pawpaw.api.application.boardImg;
 
 
+import kr.co.pawpaw.api.application.file.FileService;
 import kr.co.pawpaw.api.dto.boardImg.BoardImgDto;
 import kr.co.pawpaw.common.exception.board.BoardException;
-import kr.co.pawpaw.common.exception.board.BoardImgException;
-import kr.co.pawpaw.common.exception.board.BoardImgException.BoardImgCanNotRemoveException;
 import kr.co.pawpaw.domainrdb.board.domain.Board;
 import kr.co.pawpaw.domainrdb.board.service.query.BoardQuery;
 import kr.co.pawpaw.domainrdb.boardImg.domain.BoardImg;
 import kr.co.pawpaw.domainrdb.boardImg.service.command.BoardImgCommand;
+import kr.co.pawpaw.domainrdb.boardImg.service.query.BoardImgQuery;
+import kr.co.pawpaw.domainrdb.storage.domain.File;
+import kr.co.pawpaw.domainrdb.user.domain.UserId;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.*;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class BoardImgService {
 
-    @Value("${org.zerock.upload.path}")
-    private String uploadPath;
-
     private final BoardQuery boardQuery;
-    private final BoardImgCommand boardImgCommand;
+    private final BoardImgQuery imgQuery;
+    private final BoardImgCommand imgCommand;
+    private final FileService fileService;
 
-    public List<BoardImgDto.BoardImgResultDto> upload(Long boardId, BoardImgDto.BoardImgUploadDto uploadDto) {
-        log.info("uploadFile = {}", uploadDto);
 
-        Board board = boardQuery.findById(boardId)
-                .orElseThrow(() -> new BoardException.BoardNotFoundException());
+    @Transactional
+    public List<File> upload(Long boardId, BoardImgDto.BoardImgUploadDto uploadDto, UserId userId) {
+        Board board = boardQuery.findById(boardId).orElseThrow(BoardException.BoardNotFoundException::new);
 
-        List<BoardImgDto.BoardImgResultDto> resultList = new ArrayList<>();
+        List<BoardImg> boardImgs = uploadDto.getFiles().stream()
+                .map(file -> createBoardImg(board, file, userId))
+                .collect(Collectors.toList());
 
-        if (uploadDto.getFiles() != null) {
-            uploadDto.getFiles().forEach(multipartFile -> {
+        List<File> fileList = boardImgs.stream()
+                .map(BoardImg::getFile)
+                .collect(Collectors.toList());
 
-                boolean image = false;
-                String originalFilename = multipartFile.getOriginalFilename();
-                log.info("originalFilename = {}", originalFilename);
-                String uuid = UUID.randomUUID().toString();
-                Path savePath = Paths.get(uploadPath, uuid + "_" + originalFilename);
-
-                try {
-                    multipartFile.transferTo(savePath);
-
-                    BoardImg boardImg = BoardImg.builder()
-                            .uuid(uuid)
-                            .fileName(originalFilename)
-                            .board(board)
-                            .build();
-                    boardImgCommand.save(boardImg);
-
-                    if (Files.probeContentType(savePath).startsWith("image")) {
-                        image = true;
-//                        File thumbFile = new File(uploadPath, "s_" + uuid + "_" + originalFilename);
-//                                + extractExt(originalFilename));
-//                        Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200, 200);
-                    }
-                } catch (IOException e) {
-                    throw new BoardImgException.BoardImgCanNotUploadException();
-                }
-                resultList.add(
-                        new BoardImgDto.BoardImgResultDto(uuid, originalFilename, image)
-
-                );
-            });
-            return resultList;
+        for (BoardImg boardImg : boardImgs) {
+            imgCommand.save(boardImg);
         }
-        return null;
+
+        return fileList;
     }
 
-    public ResponseEntity<Resource> viewFileImg(String fileName) {
-        FileSystemResource resource = new FileSystemResource(uploadPath + File.separator + fileName);
-        String resourceName = resource.getFilename();
-        HttpHeaders headers = new HttpHeaders();
-
-        try {
-            headers.add("Content-Type", Files.probeContentType(resource.getFile().toPath()));
-        } catch (IOException e) {
-            throw new BoardImgException.BoardImgCanNotViewException();
-        }
-        return ResponseEntity.ok().headers(headers).body(resource);
+    @Transactional
+    public List<File> update(Long boardId, BoardImgDto.BoardImgUploadDto uploadDto, UserId userId) {
+        boardQuery.findById(boardId).orElseThrow(BoardException.BoardNotFoundException::new);
+        removeBoardImgFiles(boardId);
+        return upload(boardId, uploadDto, userId);
     }
 
+    @Transactional
+    public void removeBoardImgFiles(Long boardId) {
+        List<BoardImg> boardImgList = imgQuery.findBoardImgsWithFileByBoardId(boardId);
+        List<String> fileNameList = boardImgList.stream().map(boardImg -> boardImg.getFile().getFileName())
+                .collect(Collectors.toList());
 
-    public Map<String, Boolean> removeUploadFile(String fileName) {
-        boolean removed = false;
-        HashMap<String, Boolean> resultMap = new HashMap<>();
-        FileSystemResource resource = new FileSystemResource(uploadPath + File.separator + fileName);
-        String resourceFilename = resource.getFilename();
-
-        try {
-            removed = resource.getFile().delete();
-            String contentType = Files.probeContentType(resource.getFile().toPath());
-//            if (contentType.startsWith("image")) {
-//                File thumbFile = new File(uploadPath, "s_" + File.separator + "s_" + fileName);
-//                thumbFile.delete();
-//            }
-
-        } catch (IOException e) {
-            throw new BoardImgCanNotRemoveException();
+        for (String fileName : fileNameList) {
+            fileService.deleteFileByName(fileName);
         }
-        resultMap.put("result", removed); // 지금은 boolean 값만 반환하지만 나중에 더 다양한 값을 map에 넣어서 반환할 수 있다.
-        return resultMap;
+        for (BoardImg boardImg : boardImgList) {
+            imgCommand.delete(boardImg);
+        }
     }
 
-    private String extractExt(String originFileName) {
-        int index = originFileName.lastIndexOf(".");
-        return originFileName.substring(index);
+    public List<String> viewFileImg(Long boardId) {
+        List<BoardImg> boardImgList = imgQuery.findBoardImgsWithFileByBoardId(boardId);
+        return boardImgList.stream().map(boardImg -> fileService.getUrl(boardImg.getFile().getFileName()))
+                .collect(Collectors.toList());
+    }
+
+    private BoardImg createBoardImg(Board board, MultipartFile file, UserId userId) {
+        File savedFile = fileService.saveFileByMultipartFile(file, userId);
+        return BoardImg.builder()
+                .board(board)
+                .uuid(UUID.randomUUID().toString())
+                .file(savedFile)
+                .build();
     }
 
 }
--- api/src/main/java/kr/co/pawpaw/api/controller/boardImg/BoardImgRestController.java
+++ api/src/main/java/kr/co/pawpaw/api/controller/boardImg/BoardImgRestController.java
@@ -4,23 +4,23 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import kr.co.pawpaw.api.application.boardImg.BoardImgService;
+import kr.co.pawpaw.api.config.annotation.AuthenticatedUserId;
 import kr.co.pawpaw.api.dto.boardImg.BoardImgDto;
+import kr.co.pawpaw.domainrdb.user.domain.UserId;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/file")
 public class BoardImgRestController {
-
 
     private final BoardImgService imgService;
 
@@ -32,27 +32,26 @@ public class BoardImgRestController {
             method = "POST",
             summary = "이미지 업로드",
             description = "이미지를 업로드 한다."
-
     )
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<List<BoardImgDto.BoardImgResultDto>> upload(@RequestParam Long boardId, BoardImgDto.BoardImgUploadDto uploadDto){
-        log.info("uploadFile = {}", uploadDto);
-        List<BoardImgDto.BoardImgResultDto> upload = imgService.upload(boardId, uploadDto);
-        return ResponseEntity.ok(upload);
+    public ResponseEntity<Void> upload(@RequestParam Long boardId, @NonNull BoardImgDto.BoardImgUploadDto uploadDto, @AuthenticatedUserId UserId userId){
+        imgService.upload(boardId, uploadDto, userId);
+        return ResponseEntity.noContent().build();
     }
 
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200"),
-            @ApiResponse(responseCode = "400", description = "이미지 조회를 실패했습니다")
+            @ApiResponse(responseCode = "400", description = "이미지 수정에 실패했습니다")
     })
     @Operation(
-            method = "GET",
-            summary = "이미지 조회",
-            description = "이미지를 조회 한다."
+            method = "Patch",
+            summary = "이미지 수정",
+            description = "이미지를 수정한다."
     )
-    @GetMapping("/viewImg/{fileName}")
-    public ResponseEntity<Resource> viewImg(@PathVariable String fileName){
-        return imgService.viewFileImg(fileName);
+    @PatchMapping(value = "/update", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> update(@RequestParam Long boardId, @NonNull BoardImgDto.BoardImgUploadDto uploadDto, @AuthenticatedUserId UserId userId){
+        imgService.update(boardId, uploadDto, userId);
+        return ResponseEntity.noContent().build();
     }
 
     @ApiResponses(value = {
@@ -64,9 +63,23 @@ public class BoardImgRestController {
             summary = "이미지 삭제",
             description = "이미지를 삭제 한다."
     )
-    @DeleteMapping("/remove/{fileName}")
-    public ResponseEntity<Map<String, Boolean>> removeImg(@PathVariable String fileName){
-        Map<String, Boolean> booleanMap = imgService.removeUploadFile(fileName);
-        return ResponseEntity.ok(booleanMap);
+    @DeleteMapping("/remove")
+    public ResponseEntity<Void> removeImg(@RequestParam Long boardId){
+        imgService.removeBoardImgFiles(boardId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", description = "이미지 조회를 실패했습니다")
+    })
+    @Operation(
+            method = "GET",
+            summary = "이미지 조회",
+            description = "이미지를 조회 한다."
+    )
+    @GetMapping("/viewImg")
+    public ResponseEntity<List<String>> viewImg(@RequestParam Long boardId) {
+        return ResponseEntity.ok(imgService.viewFileImg(boardId));
     }
 }
