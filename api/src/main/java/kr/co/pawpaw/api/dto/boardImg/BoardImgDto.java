e395c7a1b8ccab11f618e14075a4ce44bbc1aa72
Seungjun Cha <chas369@naver.com>
2023-09-15 17:35:44 +0900

    #44 BoardImgResultDto 클래스 삭제

--- api/src/main/java/kr/co/pawpaw/api/dto/boardImg/BoardImgDto.java
+++ api/src/main/java/kr/co/pawpaw/api/dto/boardImg/BoardImgDto.java
@@ -15,27 +15,4 @@ public class BoardImgDto {
             this.files = files;
         }
     }
-
-    @Getter
-    public static class BoardImgResultDto {
-        private String uuid;
-        private String fileName;
-        private boolean img;
-
-
-        public BoardImgResultDto(String uuid, String fileName, boolean img) {
-            this.uuid = uuid;
-            this.fileName = fileName;
-            this.img = img;
-        }
-
-
-        public String getLink(){
-            if (img){
-                return uuid + "_" + fileName;
-            }else {
-                return "s_" + uuid + "_" + fileName;
-            }
-        }
-    }
 }
