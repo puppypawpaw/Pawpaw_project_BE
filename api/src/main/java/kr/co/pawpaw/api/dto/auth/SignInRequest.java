308df1986655bd6868992871dd412a439fbe302d
lkw15 <tnt7986@gmail.com>
2023-09-20 01:17:10 +0900

    Fix: auth controller request 예시 추가

--- api/src/main/java/kr/co/pawpaw/api/dto/auth/SignInRequest.java
+++ api/src/main/java/kr/co/pawpaw/api/dto/auth/SignInRequest.java
@@ -11,9 +11,9 @@ import javax.validation.constraints.NotBlank;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SignInRequest {
     @NotBlank
-    @Schema(description = "유저의 이메일")
+    @Schema(description = "유저의 이메일", example = "zigzaag@pawpaw.com")
     private String email;
     @NotBlank
-    @Schema(description = "유저의 비밀번호")
+    @Schema(description = "유저의 비밀번호", example = "abcd1234")
     private String password;
 }
--- api/src/main/java/kr/co/pawpaw/api/dto/auth/SocialSignUpInfoResponse.java
+++ api/src/main/java/kr/co/pawpaw/api/dto/auth/SocialSignUpInfoResponse.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 
 @Getter
 public class SocialSignUpInfoResponse {
-    @Schema(description = "소셜 유저 이름")
+    @Schema(description = "소셜 유저 이름", example = "박민수")
     private String name;
-    @Schema(description = "소셜 유저 이미지 url")
+    @Schema(description = "소셜 유저 이미지 url", example = "https://pub-2bd8949dbff24e3fbbc30ac222835ad6.r2.dev/874a7470-a648-4b99-927c-9a2acc5f65a5")
     private String profileImageUrl;
 
     private SocialSignUpInfoResponse(
