740e804a93312c2eea92beaadfa2c45b6557071c
lkw15 <tnt7986@gmail.com>
2023-09-28 11:39:28 +0900

    Fix: 유저 정보 조회 읽기 전용 Transactional 추가

--- api/src/main/java/kr/co/pawpaw/api/service/user/UserService.java
+++ api/src/main/java/kr/co/pawpaw/api/service/user/UserService.java
@@ -1,8 +1,12 @@
 package kr.co.pawpaw.api.service.user;
 
-import kr.co.pawpaw.api.service.file.FileService;
+import kr.co.pawpaw.api.dto.pet.CreatePetRequest;
+import kr.co.pawpaw.api.dto.pet.CreatePetResponse;
+import kr.co.pawpaw.api.dto.user.UpdateUserRequest;
 import kr.co.pawpaw.api.dto.user.UserResponse;
+import kr.co.pawpaw.api.service.file.FileService;
 import kr.co.pawpaw.common.exception.user.NotFoundUserException;
+import kr.co.pawpaw.domainrdb.pet.service.command.PetCommand;
 import kr.co.pawpaw.domainrdb.storage.domain.File;
 import kr.co.pawpaw.domainrdb.user.domain.User;
 import kr.co.pawpaw.domainrdb.user.domain.UserId;
@@ -19,7 +23,9 @@ import java.util.Optional;
 public class UserService {
     private final UserQuery userQuery;
     private final FileService fileService;
+    private final PetCommand petCommand;
 
+    @Transactional(readOnly = true)
     public UserResponse whoAmI(final UserId userId) {
         return userQuery.findByUserId(userId)
             .map(this::getUserResponse)
@@ -40,6 +46,28 @@ public class UserService {
             fileService.deleteFileByName(user.getUserImage().getFileName());
         }
         user.updateImage(newFile);
+    }
+
+    @Transactional
+    public void updateUser(
+        final UserId userId,
+        final UpdateUserRequest updateUserRequest
+    ) {
+        User user = userQuery.findByUserId(userId)
+            .orElseThrow(NotFoundUserException::new);
+
+        user.updateProfile(updateUserRequest.getNickname(), updateUserRequest.getBriefIntroduction());
+    }
+
+    @Transactional
+    public CreatePetResponse createPet(
+        final UserId userId,
+        final CreatePetRequest request
+    ) {
+        User user = userQuery.findByUserId(userId)
+            .orElseThrow(NotFoundUserException::new);
+
+        return CreatePetResponse.of(petCommand.save(request.toEntity(user)));
     }
 
     private UserResponse getUserResponse(final User user) {
