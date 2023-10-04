4466f24da4d8fe342b7622d90223f2f46a82ca58
lkw15 <tnt7986@gmail.com>
2023-10-04 22:48:01 +0900

    Fix: mysql chat entity 삭제

--- domain/mysql/src/main/java/kr/co/pawpaw/mysql/chatroom/repository/ChatRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.co.pawpaw.mysql.chatroom.repository;
-
-import kr.co.pawpaw.mysql.chatroom.domain.Chat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRepository extends JpaRepository<Chat, Long> {
-}
