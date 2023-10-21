01417373454e0c71424cc4535c9150d17e539401
Seungjun Cha <chas369@naver.com>
2023-10-22 00:33:54 +0900

    #90 Bookmark querydsl 구조 수정

--- domain/mysql/src/main/java/kr/co/pawpaw/mysql/bookmark/repository/BookmarkRepository.java
+++ domain/mysql/src/main/java/kr/co/pawpaw/mysql/bookmark/repository/BookmarkRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface BookmarkRepository extends JpaRepository<Bookmark, Long>, BookmarkRepositoryCustom {
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     Optional<Bookmark> deleteBookmarkByUserAndBoard(User user, Board board);
     boolean existsByUserAndBoard(User user, Board board);
